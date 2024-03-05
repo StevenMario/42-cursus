6835e598e61e6346f734627024b3ebbd74a34b25
StevenMario <stevenkoumar@gmail.com>
2024-03-05 21:06:29 +0300

    Conception du fonction ft_printf

---
+++ Projet-cursus/ft_printf/ft_printf.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: steven <steven@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 14:50:40 by mrambelo          #+#    #+#             */
+/*   Updated: 2024/03/05 10:17:35 by steven           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	int		i;
+	unsigned int	check;
+	va_list	args;
+	
+	va_start(args,format);
+	while (format[i] != '\0')
+	{
+		if(format[i] == %)
+		{
+			format[i]++;
+			if (ft_strchr("cspdiuxX",format[i]))
+			{
+				
+			}
+		}
+		else
+			ft_print_char(format[i]);
+		i++;	
+	}
+	va_end(args);
+	return (check);
+}
---
+++ Projet-cursus/ft_printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfriedri <tfriedri@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 13:36:17 by tfriedri          #+#    #+#             */
+/*   Updated: 2022/04/08 18:06:04 by tfriedri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "./libft/libft.h"
+
+int	ft_printf(const char *format, ...);
+int	ft_print_char(char c);
+
+#endif
