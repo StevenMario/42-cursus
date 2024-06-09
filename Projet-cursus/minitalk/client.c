52127c7d692f7f409e28d7af86f29ba0ac17a0ce
mrambelo <mrambelo@student.42antananarivo.mg>
2024-06-09 13:01:57 +0300

    Creation du client

--- Projet-cursus/minitalk/client.c
+++ Projet-cursus/minitalk/client.c
@@ -6,7 +6,67 @@
 /*   By: mrambelo <mrambelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 10:17:32 by mrambelo          #+#    #+#             */
-/*   Updated: 2024/06/09 10:17:33 by mrambelo         ###   ########.fr       */
+/*   Updated: 2024/06/09 13:01:44 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+void send_to_server(int pid, char c)
+{
+		int i ;
+		int shift;
+
+		i = 7;
+		while (i >= 0)
+		{
+				shift = 1 << (i);
+				if (c & shift)
+					kill(pid, SIGUSR1);
+				else 
+					kill(pid, SIGUSR2);
+				usleep(200);
+				i--;
+		}
+}
+void send_message(int pid,char *str)
+{
+	int i;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		send_to_server(pid, str[i]);
+		i++;
+	}
+	send_to_server(pid, str[i]);
+}
+
+int main(int argc , char **argv)
+{
+	int pid;
+	char *message;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		if (pid < 1)
+		{
+			ft_printf("[Erreur]. Veillez verifier votre PID!\n");
+			return (1);
+		}
+		message = argv[2];
+		if (message[0] = '\0')
+		{
+			ft_printf("[Erreur]. Aucun message n'a ete entrer!\n");
+			return (1);
+		}
+		else
+			send_message(pid,message);
+	}	
+	else
+	{
+		ft_printf("[Erreur]. Veuillez verifier le nombre d'argument!\n");
+		ft_printf("Vous devez entrer quelque chose comme ceci :./client <PID> <MESSAGE>\n");
+	}
+	return 0;
+}
---
+++ Projet-cursus/minitalk/libft/ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariosteven <mariosteven@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 13:12:45 by mrambelo          #+#    #+#             */
+/*   Updated: 2024/05/15 19:05:05 by mariosteven      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i + 1] == '-' || str[i + 1] == '+')
+			return (0);
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
