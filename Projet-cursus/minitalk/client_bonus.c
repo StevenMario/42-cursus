f496586e287a686d53076d471fff57a90ed123df
StevenMario <stevenkoumar@gmail.com>
2024-06-17 19:45:55 +0300

    Partie bonus

---
+++ Projet-cursus/minitalk/client_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariosteven <mariosteven@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 10:17:32 by mrambelo          #+#    #+#             */
+/*   Updated: 2024/06/17 19:29:52 by mariosteven      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	sighandler(int sig)
+{
+	if (sig == SIGUSR1)
+	    ft_printf("TONGA");
+}
+
+void send_to_server(int pid, char c)
+{
+		int i ;
+		int shift;
+
+		i = 7;
+		while (i >= 0)
+		{
+				shift = 1 << (i);
+				if (c & shift)
+					kill(pid, SIGUSR1);
+				else 
+					kill(pid, SIGUSR2);
+				usleep(200);
+				i--;
+		}
+		
+}
+void send_message(int pid,char *str)
+{
+	int i;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		send_to_server(pid, str[i]);
+		i++;
+	}
+	send_to_server(pid, str[i]);
+	
+}
+
+int check_pid(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		else 
+			return (0);
+	}
+	return (1);
+}
+
+int check_arg_and_send_message(char **str)
+{
+		int		pid;
+		char	*message;
+		
+
+		pid = ft_atoi(str[1]);
+		if (pid < 1 || check_pid(str[1]) == 0)
+		{
+			ft_printf("[Erreur]. Veillez verifier votre PID!\n");
+			return (0);
+		}
+		message = str[2];
+		if (message[0] == '\0')
+		{
+			ft_printf("[Erreur]. Aucun message n'a ete entrer!\n");
+			return (0);
+		}
+		else
+		{
+			send_message(pid,message);
+			return (1);
+		}
+}
+
+
+
+int main(int argc , char **argv)
+{
+    if (argc == 3)
+	{
+			if (check_arg_and_send_message(argv) == 1)	
+			{
+				printf("PID client = %d\n",getpid());
+				signal(SIGUSR1, sighandler);
+			}
+			else
+                return (1);
+	}	
+	else
+	{
+		ft_printf("[Erreur]. Veuillez verifier le nombre d'argument!\n");
+		ft_printf("Vous devez entrer quelque chose comme ceci :./client <PID> <MESSAGE>\n");
+		return (1);
+	}
+	return 0;
+}
---
+++ Projet-cursus/minitalk/server_bonus.c
@@ -0,0 +1,61 @@
+#include "minitalk.h"
+
+
+void stock_char(char c, pid_t pid)
+{
+	static char *str;
+	char tmp[2];
+
+	tmp[0] = c;
+	tmp[1] = '\0';
+	if (str == NULL)
+		str = ft_strdup("");
+	str = ft_strjoin(str,tmp);
+	if (c == '\0')
+	{
+		ft_printf("%s\n",str);
+        kill(pid,SIGUSR1);
+		free(str);
+		str = NULL;
+	}
+
+}
+
+void signal_handler(int signum, siginfo_t *info,void *context)
+{
+	static int i;
+	static char c;
+    
+
+    
+	(void)context;
+    if (signum == SIGUSR1)
+		c |= 1 << (7 - i);
+	i++;
+	if ( i > 7)
+	{
+		printf("PID client_ato amini server %d\n",info->si_pid);
+		stock_char(c,info->si_pid);
+		i = 0;
+		c = 0;
+	}
+}
+
+int    main(void)
+{
+	struct sigaction action;
+
+    action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = signal_handler;
+    sigemptyset(&action.sa_mask);
+    ft_printf("#==================================================#\n");
+	ft_printf("        Welcome to mrambelo's mintalk server        \n");
+	ft_printf("        Server PID :  %d    \n",getpid());
+	ft_printf("#==================================================#\n");
+	sigaction(SIGUSR1, &action,NULL);
+	sigaction(SIGUSR2, &action,NULL);
+	while (1)
+	{
+		
+	}
+}
