e29eb961360878a9017e01560672010a03a68814
StevenMario <stevenkoumar@gmail.com>
2024-07-29 20:45:38 +0300

    initialisation des thread

--- Projet-cursus/philosophers/philosophers.c
+++ Projet-cursus/philosophers/philosophers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrambelo <mrambelo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mariosteven <mariosteven@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 11:14:01 by mrambelo          #+#    #+#             */
-/*   Updated: 2024/07/29 13:06:01 by mrambelo         ###   ########.fr       */
+/*   Updated: 2024/07/29 20:45:10 by mariosteven      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,29 @@ int init_struct(char **argv,t_philo **philo)
 {
 	
 	*philo = malloc(sizeof(t_philo));
+	if (!*philo)
+		return (0);
 	(*philo)->info = malloc(sizeof(t_info));
+	if (!(*philo)->info)
+		return (0);
 	(*philo)->info->nb_philo = ft_atoi(argv[1]);
 	(*philo)->info->time_to_die = ft_atol(argv[2]);
 	(*philo)->info->time_to_eat = ft_atol(argv[3]);
 	(*philo)->info->time_to_sleep = ft_atol(argv[4]);
 	if (argv[5])
 		(*philo)->info->nb_of_philo_msut_eat = ft_atoi(argv[5]);
-	return 0;
+	return (1);
+}
+
+int	creat_pthread(t_philo *philo)
+{
+	int	i;
+	
+	i = 0;
+	while (i < philo->info->nb_philo)
+	{
+		pthread_create(&philo->philo,NULL,&routine,NULL);
+	}
 }
 
 int	main(int argc, char **argv)
@@ -37,7 +52,7 @@ int	main(int argc, char **argv)
 		else 
 		{
 			init_struct(argv,&philo);
-			 
+			creat_pthread(philo);
 		}
 	}
 	else
