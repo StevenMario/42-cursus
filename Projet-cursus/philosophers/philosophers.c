e966ce794f17904c84e66feb7289547565a9b393
mrambelo <mrambelo@student.42antananarivo.mg>
2024-07-31 15:18:45 +0300

    Initialisation des mutex

--- Projet-cursus/philosophers/philosophers.c
+++ Projet-cursus/philosophers/philosophers.c
@@ -3,66 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mariosteven <mariosteven@student.42.fr>    +#+  +:+       +#+        */
+/*   By: mrambelo <mrambelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 11:14:01 by mrambelo          #+#    #+#             */
-/*   Updated: 2024/07/30 18:49:09 by mariosteven      ###   ########.fr       */
+/*   Updated: 2024/07/31 15:18:22 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int init_struct(char **argv,t_philo **philo)
+int init_struct(char **argv,t_info **info)
 {
-	
-	*philo = malloc(sizeof(t_philo));
-	if (!*philo)
+	*info = malloc(sizeof(t_info));
+	if (!*info)
 		return (0);
-	(*philo)->info = malloc(sizeof(t_info));
-	if (!(*philo)->info)
-		return (0);
-	(*philo)->info->nb_philo = ft_atoi(argv[1]);
-	(*philo)->info->time_to_die = ft_atol(argv[2]);
-	(*philo)->info->time_to_eat = ft_atol(argv[3]);
-	(*philo)->info->time_to_sleep = ft_atol(argv[4]);
+	(*info)->nb_philo = ft_atoi(argv[1]);
+	(*info)->time_to_die = ft_atol(argv[2]);
+	(*info)->time_to_eat = ft_atol(argv[3]);
+	(*info)->time_to_sleep = ft_atol(argv[4]);
 	if (argv[5])
-		(*philo)->info->nb_of_philo_msut_eat = ft_atoi(argv[5]);
+		(*info)->nb_of_philo_msut_eat = ft_atoi(argv[5]);
+	(*info)->status = 0;
 	return (1);
 }
 
-void	*routine(void)
+int init_mutex(t_info **info)
 {
-	int i = 0;
-	int *mails = malloc(sizeof(int));
-	while (i < 10000000)
-		i++;
-	*mails = i;
-	return mails;
-}
+	int i;
 
-int	creat_pthread(t_philo *philo)
-{
-	int	i;
-	void *thread_retval;
 	i = 0;
-	while (i < philo->info->nb_philo)
+	while (i <= (*info)->nb_philo)
 	{
-		pthread_create(&philo->philo[i],NULL,routine,NULL);
-		i++;
+		(*info)->fork[i]; 
 	}
-	i = 0;
-	while (i < philo->info->nb_philo)
-	{
-		pthread_join(philo->philo[i],&thread_retval);
-		i++;
-	}
-	printf("mails = %d\n",*(int *)thread_retval);
-	return 0;
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo *philo;
+	t_info *info;
 	
 	if (argc == 5 || argc == 6)
 	{
@@ -70,9 +48,20 @@ int	main(int argc, char **argv)
 			printf("[Error].Please check the arguments!\n");
 		else 
 		{
-			init_struct(argv,&philo);
-			creat_pthread(philo);
-		}
+			if (!init_struct(argv,&info))
+			{
+				free(info);
+				printf("Allocation error!");
+				return (1);
+			}
+			if (!init_mutex(&info))
+			{
+				free(info);
+				printf("Allocation error!");
+				return (1);
+			}
+			
+		}	
 	}
 	else
 	{
--- Projet-cursus/philosophers/philosophers.h
+++ Projet-cursus/philosophers/philosophers.h
@@ -6,25 +6,30 @@
 # include <pthread.h>
 # include <sys/time.h>
 
+typedef struct s_philo
+{
+	int id_philo;
+	int l_fork;
+	int r_fork;
+	int	nb_eat;
+	pthread_t  	*philo;
+}	t_philo;
+
 typedef struct s_info
 {
-	int nb_philo;
+	int		nb_philo;
+	int		status;
+	long	start_time;
 	long	time_to_die;
 	long	time_to_eat;
 	long	time_to_sleep;
 	long	current_time;
-	int nb_of_philo_msut_eat;
-}	t_info;
-
-typedef struct s_philo
-{
-	int id_philo;
-	int nb_fork;
-	pthread_t  	*philo;
+	int		nb_of_philo_msut_eat;
 	pthread_mutex_t	*fork;
-	pthread_mutex_t	*print_lock;
-	t_info *info;
-}	t_philo;
+	pthread_mutex_t	print_lock;
+	pthread_mutex_t	dead_lock;
+	t_philo *philo;
+}	t_info;
 
 int	is_number(char *str);
 int	check_arg(char **argv);
@@ -32,3 +37,4 @@ int	ft_atoi(const char *str);
 long	ft_atol(const char *str);
 
 #endif
+
